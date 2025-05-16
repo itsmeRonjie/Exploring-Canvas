b8dc4dc7e1fea0972a306b23cebfa805a1f9d46e
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-16 08:39:50 +0800

    - initialized

---
+++ Exploring-Canvas/Exploring_CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exploring_CanvasApp.swift
+//  Exploring-Canvas
+//
+//  Created by Ronjie Diafante Man-on on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Exploring_CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
