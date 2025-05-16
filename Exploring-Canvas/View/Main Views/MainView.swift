3d1ce08e0a25b8bed1ac5e6af929a380c3250fa1
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-16 09:01:07 +0800

    - added drawDotsAndLines

--- Exploring-Canvas/View/Main Views/MainView.swift
+++ Exploring-Canvas/View/Main Views/MainView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct MainView: View {
     let dots: [CGPoint]
     let n = 200
-    let circleRadius = 75.0
+    let dotRadius = 2.5
+    let radiusOfInfluence = 0.3
+    
+    @State private var dragLocation: CGPoint = .zero
     
     init() {
         self.dots = (0..<n).map { _ in
@@ -30,17 +33,55 @@ struct MainView: View {
                 size in
                 let center = CGPoint(x: size.width / 2, y: size.height / 2)
                 let circleRect = CGRect(
-                    x: center.x - circleRadius,
-                    y: center.y - circleRadius,
-                    width: circleRadius * 2,
-                    height: circleRadius * 2
+                    x: center.x - dotRadius,
+                    y: center.y - dotRadius,
+                    width: dotRadius * 2,
+                    height: dotRadius * 2
                 )
                 
                 context.fill(
                     Path(ellipseIn: circleRect),
                     with: .color(.blue)
                 )
+                
+                drawDotsAndLines(
+                    context: context,
+                    size: size,
+                    dots: dots,
+                    dotRadius: dotRadius,
+                    dragLocation: dragLocation,
+                    radiusOfInfluence: radiusOfInfluence
+                )
             }
+        }
+    }
+    
+    func drawDotsAndLines(
+        context: GraphicsContext,
+        size: CGSize,
+        dots: [CGPoint],
+        dotRadius: CGFloat,
+        dragLocation: CGPoint,
+        radiusOfInfluence: CGFloat
+    ) {
+        for dot in dots {
+            let actualX = dot.x * size.width
+            let actualY = dot.y * size.height
+            let dim = dotRadius * 2
+            
+            let circle = Path(
+                ellipseIn: CGRect(
+                    x: actualX - dotRadius,
+                    y: actualY - dotRadius,
+                    width: dim,
+                    height: dim
+                )
+            )
+            
+            context.fill(
+                circle,
+                with: .color(.blue)
+            )
         }
     }
 }
